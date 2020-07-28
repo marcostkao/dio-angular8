ede97149f4968380a2ea8224b38d00503f79f671
Marcos Takao da Cunha Barbosa <MTBarbosa@rd.com.br>
2020-07-28 20:07:38 -0300

    Adicionando validações ao nosso formulário

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -12,6 +12,10 @@ export class CadastroFilmesComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
 
+  get f() {
+    return this.cadastro.controls;
+  }
+
   ngOnInit(): void {
 
     this.cadastro = this.fb.group({
@@ -26,6 +30,7 @@ export class CadastroFilmesComponent implements OnInit {
   }
 
   salvar(): void {
+    this.cadastro.markAllAsTouched();
     if (this.cadastro.invalid) {
       return;
     }
