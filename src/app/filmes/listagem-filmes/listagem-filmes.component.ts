2159c58e210931a6a788862d862eeb12d4895429
Marcos Takao da Cunha Barbosa <MTBarbosa@rd.com.br>
2020-07-30 18:48:26 -0300

    Criando nosso formulário de filtro

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 
@@ -12,11 +13,23 @@ export class ListagemFilmesComponent implements OnInit {
   readonly qtdPagina = 4;
   pagina = 0;
   filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
 
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(private filmesService: FilmesService,
+              private fb: FormBuilder
+              ) { }
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+     texto: [''],
+     genero: ['']
+    }
+    );
+
+    this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção Cientifíca', 'Comédia', 'Drama'];
+
     this.listarFilmes();
   }
 
